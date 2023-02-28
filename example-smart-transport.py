b680214b2d607df7c02c9edc6636d358f21b3193
synux0 <83602384+synux0@users.noreply.github.com>
2023-02-28 13:18:01 +0100

    Finish driver move methods

--- example-smart-transport.py
+++ example-smart-transport.py
@@ -8,66 +8,101 @@ BUS_TICKET = 1.5
 
 # Auxiliary functions
 
-def taxi_rate(dist):
-    return 1.5 + 0.5 * dist
-
 
 # Operators: They return the state (if conditions are met) or False (if conditions are not met)
 # Important: Operators CAN NOT be split up
 
-def walk(state, a, x, y):
-    if state.loc[a] == x:
-        state.loc[a] = y
+def move_the_driver_op(state, driver, next_city):
+    current_city = state.drivers[driver]['location']
+
+    if next_city in state.track_connections[current_city].keys():
+        state.drivers[driver]['location'] = next_city
         return state
     else:
         return False
 
 
-def call_taxi(state, a, x):
-    if state.wants_taxi[a]:
-        state.phoned_times[a] += 1
-        state.loc['taxi'] = x
-        return state
-    else:
-        return False
-
-
-def wait_taxi(state, a):
-    state.waiting_time[a] += 10
+def pay_bus_ticket_op(state, driver):
+    state.drivers[driver]['expenses'] += BUS_TICKET
+    
     return state
-
-
-def read_book(state, a):
-    state.read_book[a] = True
-    return state
-
-
-def ride_taxi(state, a, x, y):
-    if state.loc['taxi'] == x and state.loc[a] == x:
-        state.loc['taxi'] = y
-        state.loc[a] = y
-        state.owe[a] = taxi_rate(state.dist[x][y])
-        return state
-    else:
-        return False
-
-
-def pay_driver(state, a):
-    if state.cash[a] >= state.owe[a]:
-        state.cash[a] = state.cash[a] - state.owe[a]
-        state.owe[a] = 0
-        return state
-    else:
-        return False
 
 
 # Declaration of operators. IT IS NOT ENOUGH to define them using "def",
 # they also have to be specified to pyhop using "declare_operators".
 # Important: ALL operators must be declared in a SINGLE "declare_operators" call.
 
-pyhop.declare_operators(walk, call_taxi, wait_taxi, read_book, ride_taxi, pay_driver)
+pyhop.declare_operators(move_the_driver_op, pay_bus_ticket_op)
 print()
 pyhop.print_operators()
+
+
+def move_the_driver_method(state, driver, destination_city):
+    current_city = state.drivers[driver]['location']
+
+    if current_city != destination_city:
+        next_city = get_next_city(current_city, destination_city)
+
+        return [('move_the_driver_op', driver, next_city), ('move_the_driver', driver, destination_city)]
+    return False
+
+
+def the_driver_already_in_city(state, driver, destination_city):
+    driver_current_location = state.drivers[driver]['location']
+    
+    if driver_current_location == destination_city:
+        return []
+    return False
+
+
+pyhop.declare_methods('move_the_driver', move_the_driver_method, the_driver_already_in_city)
+
+
+def move_the_driver_by_foot(state, driver, destination_city):
+    distance = get_distance(state.drivers[driver]['location'], destination_city)
+
+    if distance < 2:
+        return [('move_the_driver', driver, destination_city)]
+    
+    return False
+
+
+def move_the_driver_by_bus(state, driver, destination_city):
+    distance = get_distance(state.drivers[driver]['location'], destination_city)
+
+    if distance >= 2:
+        return [('pay_bus_ticket_op', driver), ('move_the_driver', driver, destination_city)]
+    
+    return False
+
+
+pyhop.declare_methods('move_the_driver_to_city', move_the_driver_by_foot, move_the_driver_by_bus)
+
+
+def move_a_driver_method(state, destination_city):
+    # Search for a driver in city
+    for driver in state.drivers.keys():
+        if driver['location'] == destination_city:
+            return False
+    
+    # If no driver is present in city,
+    # get the closest one and move it to city
+    driver_to_move = find_closest_driver_to_city(destination_city)
+
+    return [('move_the_driver_to_city', driver_to_move, destination_city)]
+
+
+def a_driver_already_in_city(state, destination_city):
+    # Search for a driver in city
+    for driver in state.drivers.keys():
+        if driver['location'] == destination_city:
+            return []
+    
+    #If there was no driver found in city
+    return False
+
+
+pyhop.declare_methods('move_a_driver_to_city', move_a_driver_method, a_driver_already_in_city)
 
 
 def move_the_truck_method(state, truck, city):
@@ -76,8 +111,8 @@ def move_the_truck_method(state, truck, city):
     return [('move_the_truck_op', truck, truck_next_position), ('move_the_truck_to_city', truck, city)]
 
 
-def the_truck_already_in_city(state, truck, city):
-    if state.trucks[truck]['location'] == city:
+def the_truck_already_in_city(state, truck, destination_city):
+    if state.trucks[truck]['location'] == destination_city:
         return []
     
     return False
@@ -85,23 +120,23 @@ def the_truck_already_in_city(state, truck, city):
 pyhop.declare_methods('move_the_truck_to_city', move_the_truck_method, the_truck_already_in_city)
 
 
-def move_a_truck_method(state, city):
+def move_a_truck_method(state, destination_city):
     # Search for a truck in city
     for truck in state.trucks.keys():
-        if truck['location'] == city:
+        if truck['location'] == destination_city:
             return False
     
     # If no truck is present in city,
     # get the closest one and move it to city
-    truck_to_move = find_closest_truck_to_city(city)
+    truck_to_move = find_closest_truck_to_city(destination_city)
 
-    return [('move_the_truck_to_city', truck_to_move, city)]
+    return [('move_the_truck_to_city', truck_to_move, destination_city)]
 
 
-def a_truck_already_in_city(state, city):
+def a_truck_already_in_city(state, destination_city):
     # Search for a truck in city
     for truck in state.trucks.keys():
-        if truck['location'] == city:
+        if truck['location'] == destination_city:
             return []
     
     #If there was no truck found in city
